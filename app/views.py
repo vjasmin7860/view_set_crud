348fb10a9345c45f18926c090646b55deb12652e
vjasmin7860 <vjasmin7860@gmail.com>
2024-07-09 12:42:13 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -21,4 +21,46 @@ class ProductCrud(ViewSet):
         JO=ProductMs(PO)
         return Response(JO.data)
 
+
+
+
+    def create(self,request):
+        rjd=request.data
+        PDO=ProductMs(data=rjd)
+        if PDO.is_valid():
+            PDO.save()
+            return Response({'success':'inserted successfully'})
+        else:
+            return Response({'failed':'insertion is not done please check it once'})
+        
+
+
+    def update(self,request,pk):
+        rjd=request.data
+        instance=Product.objects.get(pid=pk)
+        LPO=ProductMs(instance,data=rjd)
+        if LPO.is_valid():
+            LPO.save()
+            return Response({'updation':'is successfully done'})
+        else:
+            return Response({'failed':'not done check once please '})
+        
+
+    def partial_update(self,request,pk):
+        rjd=request.data
+        instance=Product.objects.get(pid=pk)
+        LPO=ProductMs(instance,data=rjd,partial=True)
+        if LPO.is_valid():
+            LPO.save()
+            return Response({'updation':'is successfully done'})
+        else:
+            return Response({'failed':'not done check once please '})
+        
+
+
+    def delete(self,request,pk):# it is used for delete the given instance  
+        instance=Product.objects.get(pk=pk).delete()
+        return Response({'delete':'deleted is successfully'})
+
+
     
